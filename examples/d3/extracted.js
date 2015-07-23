dcc0e8990f6c9c8b74348b02def1ef9cff4051ab
Vlad <vlad@v-lad.org>
2015-07-22 19:34:09 -0700

    Start moving clean funs to extracted.js

---
+++ examples/d3/extracted.js
@@ -0,0 +1,14 @@
+/* jshint -W117 */
+function hashchange(t) {
+    var e = location.hash;
+    if (e && e.length > 1) {
+        e = decodeURIComponent(e.substr(1));
+        if (e !== fetcher) {
+            load(e);
+        }
+    } else {
+        if (t) {
+            load(t)
+        }
+    }
+}
