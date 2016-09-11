26e52f63114b3c7384fea69209d9f7d943420029
Vlad <vlad@v-lad.org>
2016-09-11 13:15:42 -0400

    Init TF infra

---
+++ infra/tfConfig.sh
@@ -0,0 +1,5 @@
+terraform remote config \
+    -backend=s3 \
+    -backend-config="bucket=v-lad-anim-words-tf-infra" \
+    -backend-config="key=v-lad-anim-words-tf-infra/v1" \
+    -backend-config="region=us-east-1"
