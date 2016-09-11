cc6e2731fe055196a8b2ae0788586957a1dfd939
Vlad <vlad@v-lad.org>
2016-09-11 08:55:57 -0400

    Fix build scripts

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,31 @@
 #/bin/bash
 set -e
 
+echo Building build image
+cd ci
+./ciBuild.sh
+cd ..
+
+USE_UID=$UID
+
+if [ ${USE_UID} -eq 500345588 ]; then
+    USE_UID=1000
+fi
+
+echo Using UID ${USE_UID}
+
+echo "Build D3-Cloud"
+
 NPM_VOLUME=anim-wordcloud.npm.${GO_PIPELINE_NAME}
 NODEMOD_VOLUME=anim-wordcloud.nodemod.${GO_PIPELINE_NAME}
+CONFIG_VOLUME=anim-wordcloud.config.${GO_PIPELINE_NAME}
+CACHE_VOLUME=anim-wordcloud.cache.${GO_PIPELINE_NAME}
+BOWER_VOLUME=anim-wordcloud.bower.${GO_PIPELINE_NAME}
+
 
 VOL_COMMANDS="-v $NPM_VOLUME:/app/.npm -v $NODEMOD_VOLUME:/app/node_modules"
+VOL_COMMANDS="${VOL_COMMANDS} -v $CACHE_VOLUME:/app/.cache -v $CONFIG_VOLUME:/app/.config "
+VOL_COMMANDS="${VOL_COMMANDS} -v $BOWER_VOLUME:/app/bower_components"
 
 DOCKER_VERSION=$(docker --version | sed 's/[^0-9]//g')
 if [ x${DOCKER_VERSION} == x17178629171  ]; then
@@ -14,24 +35,46 @@ if [ x${DOCKER_VERSION} == x17178629171  ]; then
    mkdir -p node_modules
 fi
 
-docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
-       $VOL_COMMANDS \
-       -i -e HOME=/app vladistan/node \
-       chown -R $UID /app/.npm
+# echo "Reset /app owner"
+#
+# docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
+#        $VOL_COMMANDS \
+#        -i -e HOME=/app local/nodebuild \
+#        chown -R $UID /app/.npm
+#
+# echo "Reset node modules owner"
+#
+# docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
+#        $VOL_COMMANDS \
+#        -i -e HOME=/app local/nodebuild \
+#        chown -R $UID /app/node_modules
+#
+# echo "Install NPM modules"
+#
+echo "AS root setup volumes"
+docker run -w /app  -v `pwd`:/app -u 0:0 \
+    $VOL_COMMANDS \
+    -i -e USE_UID=$USE_UID -e HOME=/app local/nodebuild \
+    /bin/bash ci/volPrep.sh
 
-docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u 0:0 \
-       $VOL_COMMANDS \
-       -i -e HOME=/app vladistan/node \
-       chown -R $UID /app/node_modules
 
-docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
        $VOL_COMMANDS \
-       -i -e HOME=/app vladistan/node \
-       npm install
+       -ti -e HOME=/app local/nodebuild \
+        /bin/bash
+#       npm install
 
-docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $UID:$UID \
+echo "Install NPM modules"
+docker run -w /app -v `pwd`:/app -u $USE_UID:$USE_UID \
        $VOL_COMMANDS \
-       -i -e HOME=/app vladistan/node \
+       -i -e HOME=/app local/nodebuild \
+        npm install
+
+
+echo "JS Test"
+
+docker run -w /app -v `pwd`/.m2:/.m2 -v `pwd`:/app -u $USE_UID:$USE_UID \
+       $VOL_COMMANDS \
+       -i -e HOME=/app local/nodebuild \
        gulp test
 
 sed  -i.bak  's@^SF:/app/@SF:@' report/coverage/report-lcov/lcov.info
---
+++ ci/volPrep.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+set -e
+
+echo "Preparing volumes"
+
+echo "UID to USE ${USE_UID}"
+
+BDIRS=".npm node_modules bower_components .cache .config"
+
+for d in ${BDIRS}
+do
+   chown -R ${USE_UID} ${d}
+done
